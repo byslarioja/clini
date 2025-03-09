41c2893674ab555d8122e9f4f5f7edd8d8de0239
Matias Santillan <sntlln.93@gmail.com>
2025-03-09 00:11:10 -0300

    internal: configure app boot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,12 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +25,42 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->configureModels();
+
+        $this->configureURLs();
+
+        Gate::define('viewPulse', function (User $user) {
+            return $user->email === 'sntlln.93@gmail.com';
+        });
+
+        $this->configureVite();
+    }
+
+    private function configureVite()
+    {
+        Vite::useAggressivePrefetching();
+    }
+
+    private function configureURLs()
+    {
+        if ($this->isProduction()) {
+            URL::forceScheme('https');
+        }
+    }
+
+    private function configureCommands()
+    {
+        DB::prohibitDestructiveCommands($this->isProduction());
+    }
+
+    private function configureModels()
+    {
+        Model::shouldBeStrict();
+        Model::unguard();
+    }
+
+    private function isProduction()
+    {
+        return $this->app->environment() === 'production';
     }
 }
