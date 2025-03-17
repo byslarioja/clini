54e099f4f8987ae5a14e31a8c4e3566851f5dee0
Matias Santillan <sntlln.93@gmail.com>
2025-03-17 02:48:28 +0000

    feat: add hint component

---
+++ resources/js/components/input-hint.tsx
@@ -0,0 +1,10 @@
+import { cn } from '@/lib/utils';
+import { HTMLAttributes } from 'react';
+
+export default function InputHint({ message, className = '', ...props }: HTMLAttributes<HTMLParagraphElement> & { message?: string }) {
+    return message ? (
+        <p {...props} className={cn('text-sm text-gray-500 dark:text-gray-100', className)}>
+            {message}
+        </p>
+    ) : null;
+}
