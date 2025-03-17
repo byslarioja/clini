ed87b5e6239f297ac5be44261bd5115f601489e0
Matias Santillan <sntlln.93@gmail.com>
2025-03-17 06:51:43 +0000

    feat: add sonner popover when success

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -41,6 +41,7 @@ class HandleInertiaRequests extends Middleware
 
         return [
             ...parent::share($request),
+            'message' => $request->session()->get('message'),
             'name' => config('app.name'),
             'quote' => ['message' => trim($message), 'author' => trim($author)],
             'auth' => [
--- resources/js/types/global.d.ts
+++ resources/js/types/global.d.ts
@@ -1,5 +1,10 @@
+import type { Page } from '@inertiajs/core';
 import type { route as routeFn } from 'ziggy-js';
 
 declare global {
     const route: typeof routeFn;
+}
+
+declare module '@inertiajs/react' {
+    export declare function usePage<T>(): Page<T & { message?: { content: string; type: 'error' | 'info' | 'success' } }>;
 }
