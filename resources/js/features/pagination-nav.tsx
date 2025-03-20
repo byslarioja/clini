e3b5a58a7c7d821fc1485a8596766074139ef539
Matias Santillan <sntlln.93@gmail.com>
2025-03-20 06:55:15 +0000

    feat: add table pagination component

---
+++ resources/js/features/pagination-nav.tsx
@@ -0,0 +1,40 @@
+import { Pagination, PaginationContent, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from '@/components/ui/pagination';
+import { PaginatedLink } from '@/types';
+
+export function PaginationNav({ links }: { links: PaginatedLink[] }) {
+    return (
+        <Pagination>
+            <PaginationContent>
+                {links.map((link) => (
+                    <PaginatedItem key={link.label} link={link} />
+                ))}
+            </PaginationContent>
+        </Pagination>
+    );
+}
+
+function PaginatedItem({ link }: { link: PaginatedLink }) {
+    if (link.label.includes('Anterior')) {
+        return (
+            <PaginationItem key={link.label}>
+                <PaginationPrevious href={link.url}>{link.label}</PaginationPrevious>
+            </PaginationItem>
+        );
+    }
+
+    if (link.label.includes('Siguiente')) {
+        return (
+            <PaginationItem key={link.label}>
+                <PaginationNext href={link.url}>{link.label}</PaginationNext>
+            </PaginationItem>
+        );
+    }
+
+    return (
+        <PaginationItem key={link.label}>
+            <PaginationLink href={link.url} isActive={link.active}>
+                {link.label}
+            </PaginationLink>
+        </PaginationItem>
+    );
+}
