0e9b0c3f06e3eb70ee3d5e15a8a6674bae2ef83b
Matias Santillan <sntlln.93@gmail.com>
2025-03-21 04:59:59 +0000

    feat: add patients table

--- app/Http/Controllers/Backoffice/PatientController.php
+++ app/Http/Controllers/Backoffice/PatientController.php
@@ -3,15 +3,27 @@
 namespace App\Http\Controllers\Backoffice;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Backoffice\PaginatedPatientsRequest;
 use App\Http\Resources\Backoffice\PatientResource;
 use App\Models\Patient;
 use Inertia\Inertia;
 
 class PatientController extends Controller
 {
-    public function index(): \Inertia\Response
+    public function index(PaginatedPatientsRequest $request): \Inertia\Response
     {
-        $patients = Patient::paginate(20);
+        $query = $request->validated();
+
+        $patients = Patient::query()
+            ->where(function ($q) use ($query) {
+                $search = $query['filter'];
+
+                return $q->where('name', 'like', "%$search%")
+                    ->orWhere('dni', 'like', "%$search%")
+                    ->orWhere('phone', 'like', "%$search%");
+            })
+            ->orderBy($query['sort_column'], $query['sort_order'])
+            ->paginate($query['per_page'], page: $query['page']);
 
         return Inertia::render('patients/index', [
             'patients' => PatientResource::collection($patients),
--- resources/js/pages/patients/index.tsx
+++ resources/js/pages/patients/index.tsx
@@ -3,9 +3,11 @@ import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, Tabl
 import { PaginationNav } from '@/features/pagination-nav';
 import { Searchbar } from '@/features/searchbar';
 import AppLayout from '@/layouts/app-layout';
+import table from '@/lib/services/table';
 import { Paginated, type BreadcrumbItem } from '@/types';
+import { type Patient } from '@/types/entities';
 import { Head, Link } from '@inertiajs/react';
-import { Edit2, Trash } from 'lucide-react';
+import { ArrowUpDown, Edit2, Plus, Trash } from 'lucide-react';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -18,34 +20,27 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
 ];
 
-interface Patient {
-    id: number;
-    name: string;
-    phone: string;
-    sex: string;
-    dni: string;
-    dob: string;
-}
-
 export default function Patients({ patients }: Paginated<Patient>) {
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Pacientes" />
-            <div className="mb-4 flex justify-between px-6 py-8 md:px-4">
+            <div className="my-8 mb-4 flex justify-between px-6 md:px-4">
                 <Searchbar indexRoute="patients.index" />
-
-                {/* <Button asChild>
-                        <Link href={route('stockables.create')}>
-                            <Plus />
-                            Agregar stockeable
-                        </Link>
-                    </Button> */}
+                <Link href={route('home')} className={buttonVariants()}>
+                    <Plus />
+                    Nuevo paciente
+                </Link>
             </div>
-            <main className="max-w-screen items-center px-6 py-8 md:px-4">
+            <main className="max-w-screen items-center px-6 pb-8 md:px-4">
                 <Table>
                     <TableHeader>
                         <TableRow>
-                            <TableHead>Nombre</TableHead>
+                            <TableHead className="flex items-center gap-2">
+                                <button onClick={() => table.sort('name', 'patients.index')}>
+                                    <ArrowUpDown className="h-4 w-4" />
+                                </button>
+                                Nombre
+                            </TableHead>
                             <TableHead>Teléfono</TableHead>
                             <TableHead>Sexo</TableHead>
                             <TableHead>Documento</TableHead>
